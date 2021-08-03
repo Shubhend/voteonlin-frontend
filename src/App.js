ec700e900b859814ee7924ff43a42178d4758060
xuark <admin@xuark.com>
2021-08-03 21:43:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -2,15 +2,44 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Component/Header/header';
 import Indexpage from "./Pages";
+import Details from "./Pages/detail";
 import Footer from './Component/Footer/footer';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
      <Header/>
-     <Indexpage/>
+
+        <Router>
+        <Switch>
+            <Route path="/about">
+                <Details />
+            </Route>
+            <Route path="/details" exact>
+               <Details/>
+            </Route>
+            <Route path="/">
+                <Indexpage/>
+            </Route>
+        </Switch>
+        </Router>
     <Footer/>
+
+
+
+
     </div>
+
+
+
+
+
   );
 }
 
